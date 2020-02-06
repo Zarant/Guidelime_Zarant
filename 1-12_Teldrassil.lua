0d833c17c890c88fd8394a499f0c890aa60d37cc
Zarant <26726709+Zarant@users.noreply.github.com>
2020-02-06 02:27:02 -0300

    hunter specific notes

--- 1-12_Teldrassil.lua
+++ 1-12_Teldrassil.lua
@@ -154,6 +154,7 @@ Speak to the NPC patrolling the road\\Turn in [QT487 The Road to Darnassus]
 [G31.53,31.61Teldrassil]Accept [QA938 Mist]
 [G38.31,34.36Teldrassil]Turn in [QT937 The Enchanted Glade]
 Turn in [QT938 Mist] \\Accept [QA940 Teldrassil]
+Head north and tame a level 11 Feral Nightsaber[A Hunter]
 Kill yourself and spirit rez at darnassus [OC]
 
 
